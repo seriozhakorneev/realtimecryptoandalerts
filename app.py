480fa78aaad9bf4d90adb7fe59ddff8c648b24be
Sergei <seriozhakorneev@gmail.com>
2021-04-08 18:37:17 +0300

    commit

--- app.py
+++ app.py
@@ -4,17 +4,39 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi_utils.tasks import repeat_every
 from fastapi.staticfiles import StaticFiles
 
-
-from ast import literal_eval 
 import uvicorn
+
+from ast import literal_eval
 import api
 
-
+# config
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# help functions
+def control_cookie_size(cookie_string):
+    # return True if cookies 
+    # reached their limit(4kb)
+    return len(cookie_string.encode('utf-8')) > 4000
 
+async def check_alerts(alerts_dict, price, s_pair):
+    # return alert and change alert_values
+    alert_values = alerts_dict[s_pair]
+
+    if (alert_values[0] != None) and (alert_values[0] <= float(price)):
+        raise_alert = s_pair, alert_values[0]
+        alerts_dict[s_pair][0] = None
+        return raise_alert
+
+    elif (alert_values[1] != None) and (alert_values[1] >= float(price)):
+        raise_alert = s_pair, alert_values[1]
+        alerts_dict[s_pair][1] = None
+        return raise_alert
+
+    return [None]
+
+# routes
 @app.on_event("startup")
 @repeat_every(seconds=1)
 def make_api_request_and_save_tokens():
@@ -26,79 +48,66 @@ def make_api_request_and_save_tokens():
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    # get cookies
+    session_pairs = request.cookies.get('pairs')
+    alerts = request.cookies.get('alerts')
     session_pairs_list = []
+    alerts_dict = {}
     p_names = []
+    # read prices file
     pairs = await api.read_json_file()
-
+    
     if pairs:
         # list of all pair names for <datalist/>
         p_names = pairs.keys()
-
-    session_pairs = request.cookies.get('pairs')
     if session_pairs:
-        # from str to list
+        # str to list
         session_pairs_list = session_pairs.split(" ")
+    if alerts:
+        # str to dict
+        alerts_dict = literal_eval(alerts)
        
     return templates.TemplateResponse("index.html", {
         "request": request,
         "session_pairs_list": session_pairs_list,
+        "alerts_dict": alerts_dict,
         "p_names": p_names,
         })
 
 
-@app.get('/crt')
-async def crt(response: Response):
-
-    alerts = {'BTCUSDT' : 15}
-    response.set_cookie(key="alerts", value=str(alerts)) 
-    
-    return 'success'
-
-@app.get('/add_alert')
-async def add_alert(request: Request, response: Response):
-
-    alert_pair = 'BNBUSDT'
-    alert_price = 17
-
-    alerts = request.cookies.get('alerts')
-    if alerts:
-        # str to dict
-        alerts_dict = literal_eval(alerts)
-        alerts_dict[alert_pair] = alert_price
-
-    # dict to str
-    response.set_cookie(key="alerts", value=str(alerts_dict))
-
-    return alerts_dict
-
-@app.get('/get_alert')
-async def get_alert(request: Request, response: Response):
-
-    alerts = request.cookies.get('alerts')
-    if alerts:
-        # str to dict
-        alerts_dict = literal_eval(alerts)
-
-    return alerts_dict
-
-
 @app.get('/data')
-async def data(request: Request):
+async def data(request: Request,response: Response,):
     result = []
     raise_alert = [None]
     pairs = await api.read_json_file()
     session_pairs = request.cookies.get('pairs')
-
+    alerts = request.cookies.get('alerts')
+    
     if session_pairs and pairs:
         session_pairs_list = session_pairs.split(" ")
         # get pairs prices
         for s_pair in session_pairs_list:
             price = pairs.get(s_pair)
-            # если цена совпадает с алертом то
-            # raise_alert = s_pair, alert
             result.append(price)
 
-    #raise_alert = 'BTCUSDT', '0.0086739'
+            # alerts
+            if alerts:
+                alerts_dict = literal_eval(alerts)
+                if s_pair in alerts_dict.keys():
+                    raise_alert = await check_alerts(alerts_dict, price, s_pair)
+
+                    # if alert raised, change cookie
+                    if raise_alert[0]:
+                        # delete pair if no alerts left
+                        if alerts_dict[s_pair].count(None) == 2:
+                            del alerts_dict[s_pair]
+                        # save to cookie
+                        response.set_cookie(
+                            key="alerts",
+                            value=str(alerts_dict),
+                            expires=1209600)
+                            # 2 weeks in seconds
+    
     return {
          'result': result,
          'raise_alert': raise_alert,
@@ -109,12 +118,14 @@ async def data(request: Request):
 async def add_pair(request: Request, trade_pair: str = Form(...)):
     
     session_pairs = request.cookies.get('pairs')
+
+    if control_cookie_size(session_pairs):
+        return "Your cookies reached their limit of 4kb."
+
     if session_pairs:
-        # from str to list
         session_pairs_list = session_pairs.split(" ")
         # add if already not in list
         if trade_pair not in session_pairs_list:
-            # add new pair
             session_pairs_list.append(trade_pair)
     else:
         # create new if cookie empty
@@ -123,8 +134,42 @@ async def add_pair(request: Request, trade_pair: str = Form(...)):
      # redirect to index, and set new cookie
     redirect_resp = RedirectResponse('/', status_code=303)
     session_pairs_string = ' '.join(session_pairs_list)
-    redirect_resp.set_cookie(key="pairs", value=session_pairs_string)
+    redirect_resp.set_cookie(key="pairs", value=session_pairs_string, expires=1209600)
+    return redirect_resp
 
+
+@app.post('/add_alert')
+async def add_alert(
+    request: Request,
+    trade_pair: str = Form(...),
+    alert_val: float = Form(...),
+    direction: str = Form(...)):
+
+    values = [None, None]
+    alerts_dict = {}
+    req_alerts = request.cookies.get('alerts')
+    
+    if control_cookie_size(req_alerts):
+        return "Your cookies reached their limit of 4kb."
+
+    # radiobutton index for values list
+    if direction == "up":
+        a_index = 0
+    elif direction == "down":
+        a_index = 1
+    
+    if req_alerts:
+        alerts_dict = literal_eval(req_alerts)
+        # if pair is in cookie
+        current_values = alerts_dict.get(trade_pair)
+        if current_values:
+            values = current_values  
+
+    values[a_index] = alert_val
+    alerts_dict[trade_pair] = values
+
+    redirect_resp = RedirectResponse('/', status_code=303)
+    redirect_resp.set_cookie(key="alerts", value=str(alerts_dict), expires=1209600)
     return redirect_resp
 
 
@@ -132,68 +177,52 @@ async def add_pair(request: Request, trade_pair: str = Form(...)):
 async def delete_pair(request: Request, trade_pair: str = Form(...)):
     
     session_pairs = request.cookies.get('pairs')
+
     if session_pairs:
         session_pairs_list = session_pairs.split(" ")
         # delete pair
         session_pairs_list.remove(trade_pair)
+        
+        alerts = request.cookies.get('alerts')
+        if alerts:
+            alerts_dict = literal_eval(alerts)
+       
+        values = alerts_dict.get(trade_pair)
+        if values:
+            # delete alert pair 
+            del alerts_dict[trade_pair]
 
-        # redirect to index, and set new cookie
         redirect_resp = RedirectResponse('/', status_code=303)
         session_pairs_string = ' '.join(session_pairs_list)
-        redirect_resp.set_cookie(key="pairs", value=session_pairs_string)
-
+        redirect_resp.set_cookie(key="pairs", value=session_pairs_string, expires=1209600)
+        redirect_resp.set_cookie(key="alerts", value=str(alerts_dict), expires=1209600)
     return redirect_resp
 
 
+@app.post('/delete_alert')
+async def delete_alert(
+    request: Request,
+    trade_pair: str = Form(...),
+    alert_index: int = Form(...),):
+
+    req_alerts = request.cookies.get('alerts')
+
+    if req_alerts:
+        alerts_dict = literal_eval(req_alerts)
+        
+        values = alerts_dict.get(trade_pair)
+        if values:
+            # delete alert
+            values[alert_index] = None
+            alerts_dict[trade_pair] = values
+            
+            # delete pair if no alerts left
+            if values.count(None) == 2:
+                del alerts_dict[trade_pair]
+    
+    redirect_resp = RedirectResponse('/', status_code=303)
+    redirect_resp.set_cookie(key="alerts", value=str(alerts_dict), expires=1209600)
+    return redirect_resp
+
 if __name__ == "__main__":
     uvicorn.run("app:app")# reload=True
-
-
-# how much cookie i can put in key
-"""abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghi
-jklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrst
-uvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdef
-ghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr
-stuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcde
-fghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrs
-tuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghi
-jklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy
-zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopq
-rstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij
-klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd
-efghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuv
-wxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd
-efghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij
-klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr
-stuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyza
-bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl
-mnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij
-klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk
-lmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnop
-qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh
-ijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyza
-bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstu
-vwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnop
-qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl
-mnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghij
-klmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh
-ijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg
-hijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg
-hijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh
-ijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk
-lmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmn
-opqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr
-stuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzab
-cdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh
-ijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmno
-pqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw
-xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmn
-"""
